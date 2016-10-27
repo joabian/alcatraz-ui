12b56de8ae759469263d0600838445064b95fd80
Janethiscoming <janethiscoming@outlook.com>
2016-10-27 13:38:54 -0600

    Add files via upload

---
+++ App_Code/mySqlConn.vb
@@ -0,0 +1,94 @@
+﻿Imports Microsoft.VisualBasic
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports System.Data
+
+Public Class mySqlConn
+
+    Public connString As String = "Server=localhost;Database=db_alcatraz;Uid=dev;Pwd=shaggy;"
+
+    Public conn As MySqlConnection
+    Public SUCCESS As String = "SUCCESS"
+    Public SERROR As String = "ERROR"
+
+
+    Public Function executeSQL(ByVal sSql As String, ByRef sResult As String) As Data.DataTable
+        Dim sReturn As String = ""
+        'Dim sr As SqlDataReader = Nothing
+        Dim dt As DataTable = New DataTable
+        Dim da As New MySqlDataAdapter
+        conn = New MySqlConnection
+        Try
+            conn.ConnectionString = connString
+            conn.Open()
+            Dim sComm As New MySqlCommand
+            sComm.CommandText = sSql
+            sComm.Connection = conn
+            da.SelectCommand = sComm
+            da.Fill(dt)
+            conn.Close()
+            sResult = SUCCESS
+        Catch ex As Exception
+            sResult = SERROR & ": " & ex.Message
+            If (conn.State = Data.ConnectionState.Open) Then
+                conn.Close()
+            End If
+            MsgBox(sResult)
+        End Try
+        conn = Nothing
+        Return dt
+    End Function
+    Public Function executeDMLSQL(ByVal sSql As String, ByRef sResult As String) As Integer
+        Dim sReturn As String = ""
+        Dim irows As Integer = 0
+        conn = New MySqlConnection
+        Try
+            conn.ConnectionString = connString
+            conn.Open()
+            Dim sComm As New MySqlCommand
+            sComm.CommandText = sSql
+            sComm.Connection = conn
+            irows = sComm.ExecuteNonQuery()
+            conn.Close()
+            sResult = SUCCESS
+        Catch ex As Exception
+            sResult = SERROR & ": " & ex.Message
+            If (conn.State = Data.ConnectionState.Open) Then
+                conn.Close()
+            End If
+            MsgBox(sResult)
+        End Try
+        conn = Nothing
+        Return irows
+    End Function
+
+    Public Function executeSQL_dset(ByVal sSql As String, ByRef sResult As String) As Data.DataSet
+        Dim sReturn As String = ""
+        'Dim sr As SqlDataReader = Nothing
+        'Dim dt As DataTable = New DataTable
+        Dim dt As DataSet = New DataSet
+        Dim da As New MySqlDataAdapter
+        conn = New MySqlConnection
+        Try
+            conn.ConnectionString = connString
+            conn.Open()
+            Dim sComm As New MySqlCommand
+            sComm.CommandText = sSql
+            sComm.Connection = conn
+            da.SelectCommand = sComm
+            da.Fill(dt)
+            conn.Close()
+            sResult = SUCCESS
+        Catch ex As Exception
+            sResult = SERROR & ": " & ex.Message
+            If (conn.State = Data.ConnectionState.Open) Then
+                conn.Close()
+            End If
+            MsgBox(sResult)
+        End Try
+        conn = Nothing
+        Return dt
+    End Function
+
+
+End Class
---
+++ App_Code/reports.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Web.Script.Serialization
+
+Public Class reports
+
+    Public mysql As New mySqlConn()
+    Public query As String
+    Public query_result As String
+    Public ds As DataSet
+
+
+    Function ReportUsuariosConHuellas() As DataSet
+        'query = "select usrs.numero_matricula,usrs.name,date(usrs.date_update) as date_update,usrs.username_update,fp.quality,fp.finger"
+        'query += " from tb_user_fingerprint fp inner join sma_companies usrs on fp.customer_id = usrs.id"
+        ' query += " order by date_update desc"
+        'query += " limit 100"
+        query = "SELECT name, first_name, last_name, phone, email  FROM db_alcatraz.sma_companies"
+        query += " limit 100"
+        ds = mysql.executeSQL_dset(query, query_result)
+
+        Return ds
+    End Function
+
+
+    Function testData() As DataSet
+        query = "select numero_matricula, name, id as date_update, id as quality, numero_matricula as finger, numero_matricula as username_update from sma_companies where numero_matricula is not null and numero_matricula <> '' limit 50"
+        ds = mysql.executeSQL_dset(query, query_result)
+
+        Return ds
+    End Function
+
+
+    Function DataSetToJSON(ds As DataSet) As String
+        Dim dict As New Dictionary(Of String, Object)
+
+        For Each dt As DataTable In ds.Tables
+            Dim arr(dt.Rows.Count) As Object
+
+            For i As Integer = 0 To dt.Rows.Count - 1
+                arr(i) = dt.Rows(i).ItemArray
+            Next
+
+            dict.Add(dt.TableName, arr)
+        Next
+
+        Dim json As New JavaScriptSerializer
+        Return json.Serialize(dict)
+    End Function
+
+End Class
